354f90dfb72de990401d537005674d5424f7583c
abu bokor suraj <abusurajbd@gmail.com>
2022-09-10 14:10:17 +0600

    error fixed

---
+++ js/utilities.js
@@ -0,0 +1,26 @@
+function getInputFieldValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputFieldValueString = inputField.value;
+  inputField.value = "";
+  const inputFieldValue = parseFloat(inputFieldValueString);
+  // console.log(typeof inputFieldValueString)
+  if(inputFieldValue >0){
+    return inputFieldValue;}
+
+    alert("please input a valid number");
+    return;
+  
+}
+
+function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const textElementValueString = textElement.innerText;
+  const textElementValue = parseFloat(textElementValueString);
+  console.log(textElementValue);
+  return textElementValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.innerText = newValue;
+}
